1b2a2a73189837ed644c22642d6f5564036c9b3c
chiragworkbench <136353738+chiragworkbench@users.noreply.github.com>
2025-03-18 12:06:51 +0530

    Fix requirements

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/sales_data')
-def get_sales_data():
-    df = pd.read_csv('sales_data.csv')  # Load data from CSV
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/sales_data")
+def sales_data():
     data = {
-        "month": df["month"].tolist(),
-        "sales": df["sales"].tolist()
+        "month": ["Jan", "Feb", "Mar"],
+        "sales": [1000, 1500, 2000]
     }
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000, debug=True)
